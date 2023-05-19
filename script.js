1299af614f982610854b7d6484dc11bb56b9d2e9
Gleb Leus <lv.gleb44@gmail.com>
2023-05-19 22:56:42 +0300

    section best items

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const changePhoto = () => {
+    const photo = document.querySelector('.best-items__image-for-more > img')
+
+    if (window.innerWidth < 376) {
+        photo.setAttribute('src', './images/BestItems/image 23.2.png');
+    } else {
+        photo.setAttribute('src', './images/BestItems/image 23.png');
+    }
+}
+
 // ---HAMBURGER-MENU----------------------
 const hamburgerMenu = () => {
     const menu = document.querySelector('.nav-header'),
@@ -22,5 +32,43 @@ const dropdownMenu = () => {
     })
 }
 
+// ---SLIDER-------------------------------
+
+const slider = () => {
+    const slider = document.querySelector('.slider-bestitems'),
+          slides = document.querySelector('.slider-besttimes__slides'),
+          slidesArray = document.querySelectorAll('.slider-bestitems__image'),
+          nextBtn = document.querySelector('.offer-bestitems__arrow'),
+          dotes = document.querySelectorAll('.slider-bestitems__dote');
+
+    const slidesWidth = slides.clientWidth,
+          end = slidesWidth * (slidesArray.length - 1);
+    
+    let currentPosition = 0,
+        activeDoteNumber = 0;
+
+    nextBtn.addEventListener('click', () => {
+        dotes.forEach(dote => {
+            dote.classList.remove('active');
+        })
+
+        if (currentPosition == end) {
+            currentPosition = 0;
+            activeDoteNumber = 0
+            dotes[activeDoteNumber].classList.add('active');
+            slides.style.transform = `translateX(-${currentPosition}px)`;
+            return;
+        }
+
+        currentPosition += slidesWidth;
+        activeDoteNumber++;
+        slides.style.transform = `translateX(-${currentPosition}px)`;
+
+        dotes[activeDoteNumber].classList.add('active');
+    })
+}
+
+slider();
 dropdownMenu();
 hamburgerMenu();
+changePhoto();
