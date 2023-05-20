db6e18f2954b27a40e7908caed509728fda54963
Gleb Leus <lv.gleb44@gmail.com>
2023-05-20 20:39:19 +0300

    catalog

--- script.js
+++ script.js
@@ -1,74 +1,61 @@
-const changePhoto = () => {
-    const photo = document.querySelector('.best-items__image-for-more > img')
 
-    if (window.innerWidth < 376) {
-        photo.setAttribute('src', './images/BestItems/image 23.2.png');
-    } else {
-        photo.setAttribute('src', './images/BestItems/image 23.png');
-    }
+const photo = document.querySelector('.best-items__image-for-more > img')
+
+if (window.innerWidth < 376) {
+    photo.setAttribute('src', './images/BestItems/image 23.2.png');
+} else {
+    photo.setAttribute('src', './images/BestItems/image 23.png');
 }
 
 // ---HAMBURGER-MENU----------------------
-const hamburgerMenu = () => {
-    const menu = document.querySelector('.nav-header'),
-      menuBtn = document.querySelector('.header__hamb-btn');
+const hambMenu = document.querySelector('.nav-header'),
+    hambMenuBtn = document.querySelector('.header__hamb-btn');
 
-    menuBtn.addEventListener('click', () => {
-        menu.classList.toggle('active');
-    });
-}
+    hambMenuBtn.addEventListener('click', () => {
+    hambMenu.classList.toggle('active');
+});
 
 // ---DROPDOWN-MENU-----------------------
 
-const dropdownMenu = () => {
-    const menu = document.querySelector('.header__dropdown'),
-          menuBtn = document.querySelector('.btns-header__item_dropdown-btn');
+const menu = document.querySelector('.header__dropdown'),
+        menuBtn = document.querySelector('.btns-header__item_dropdown-btn');
 
-    menuBtn.addEventListener('mouseover', () => {
-        menu.classList.remove('inactive');
-    });
-    menuBtn.addEventListener('mouseout', () => {
-        menu.classList.add('inactive');
-    })
-}
+menuBtn.addEventListener('mouseover', () => {
+    menu.classList.remove('inactive');
+});
+menuBtn.addEventListener('mouseout', () => {
+    menu.classList.add('inactive');
+})
 
 // ---SLIDER-------------------------------
 
-const slider = () => {
-    const slider = document.querySelector('.slider-bestitems'),
-          slides = document.querySelector('.slider-besttimes__slides'),
-          slidesArray = document.querySelectorAll('.slider-bestitems__image'),
-          nextBtn = document.querySelector('.offer-bestitems__arrow'),
-          dotes = document.querySelectorAll('.slider-bestitems__dote');
+const slides = document.querySelector('.slider-besttimes__slides'),
+        slidesArray = document.querySelectorAll('.slider-bestitems__image'),
+        nextBtn = document.querySelector('.offer-bestitems__arrow'),
+        dotes = document.querySelectorAll('.slider-bestitems__dote');
 
-    const slidesWidth = slides.clientWidth,
-          end = slidesWidth * (slidesArray.length - 1);
-    
-    let currentPosition = 0,
-        activeDoteNumber = 0;
+const slidesWidth = slides.clientWidth,
+        end = slidesWidth * (slidesArray.length - 1);
 
-    nextBtn.addEventListener('click', () => {
-        dotes.forEach(dote => {
-            dote.classList.remove('active');
-        })
+let currentPosition = 0,
+    activeDoteNumber = 0;
 
-        if (currentPosition == end) {
-            currentPosition = 0;
-            activeDoteNumber = 0
-            dotes[activeDoteNumber].classList.add('active');
-            slides.style.transform = `translateX(-${currentPosition}px)`;
-            return;
-        }
-
-        currentPosition += slidesWidth;
-        activeDoteNumber++;
-        slides.style.transform = `translateX(-${currentPosition}px)`;
-
-        dotes[activeDoteNumber].classList.add('active');
+nextBtn.addEventListener('click', () => {
+    dotes.forEach(dote => {
+        dote.classList.remove('active');
     })
-}
 
-slider();
-dropdownMenu();
-hamburgerMenu();
-changePhoto();
+    if (currentPosition == end) {
+        currentPosition = 0;
+        activeDoteNumber = 0
+        dotes[activeDoteNumber].classList.add('active');
+        slides.style.transform = `translateX(-${currentPosition}px)`;
+        return;
+    }
+
+    currentPosition += slidesWidth;
+    activeDoteNumber++;
+    slides.style.transform = `translateX(-${currentPosition}px)`;
+
+    dotes[activeDoteNumber].classList.add('active');
+})
